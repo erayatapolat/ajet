b9e2ed9af0b0c20b9e573939fd04b7b8b7f1e0d9
erayatapolat <erayataplt_08@outlook.com>
2025-06-15 23:22:50 +0300

    .

--- src/component/ui/header.tsx
+++ src/component/ui/header.tsx
@@ -1,13 +1,16 @@
+"use client"
 import Image from "next/image";
 import Ajet from "../../../public/ajet.svg";
 import LanguageSelection from "../dropdown/language";
 import AuthButton from "../AuthButton";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+    const route = useRouter();
     return (
         <header className="w-full mt-4 fixed top-0 z-50">
             <div className="mx-auto w-full max-w-7xl h-[70px] flex items-center justify-between rounded-2xl bg-gradient-to-r from-[#0090FF] to-[#002B5B] relative overflow-visible px-4 md:px-8">
-                <div className="flex items-center">
+                <div className="flex items-center" onClick={()=>(route.push('/'))}>
                     <Image src={Ajet} alt="Ajet Logo" height={36} className="h-9 w-auto" />
                 </div>
                 <nav className="hidden md:flex gap-10 text-white text-base font-medium">
